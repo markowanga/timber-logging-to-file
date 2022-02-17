b32f01c6da315f57930dc82bd9b0394535ffc35e
Marcin Wątroba <markowanga@gmail.com>
2022-02-18 00:19:32 +0100

    Add Jitpack java config

--- app/src/androidTest/java/pl/theliver/timberloggingtofileapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/pl/theliver/timberloggingtofileapp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("pl.theliver.timberloggingtofile", appContext.packageName)
+        assertEquals("pl.theliver.timberloggingtofileapp", appContext.packageName)
     }
 }
