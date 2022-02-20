2405cc8a9b2528822fcf676cbdda7c78e31fe4b5
murt <piskorski.pio@gmail.com>
2022-02-20 13:29:54 +0100

    review suggestion - factory improvement

--- library/src/main/java/com/github/markowanga/timberloggingtofile/LogToFileFactory.kt
+++ library/src/main/java/com/github/markowanga/timberloggingtofile/LogToFileFactory.kt
@@ -1,5 +1,7 @@
 package com.github.markowanga.timberloggingtofile
 
+import com.github.markowanga.timberloggingtofile.crypt.Base64TextCrypt
+import com.github.markowanga.timberloggingtofile.crypt.CipherTextCrypt
 import com.github.markowanga.timberloggingtofile.crypt.TextCrypt
 import java.io.File
 import java.time.format.DateTimeFormatter
@@ -7,7 +9,19 @@ import java.time.format.DateTimeFormatter
 class LogToFileFactory(
     private val logsDirectory: File,
 ) {
-    fun create(
+    fun createPlainTextTree() = create( logFilePrefix = "plain_text_")
+
+    fun createBase64Tree() = create(
+        Base64TextCrypt(),
+        logFilePrefix = "base64_"
+    )
+
+    fun createCipherTree() = create(
+        CipherTextCrypt("test1234test1234"),
+        logFilePrefix = "cipher_"
+    )
+
+    private fun create(
         textCrypt: TextCrypt? = null,
         logFilePrefix: String = DEFAULT_LOG_FILE_PREFIX,
         logFileExtension: String = DEFAULT_EXTENSION,
